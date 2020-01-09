b954248ff0184742bf46882037770e423de9ccdf
mlukac2 <matteo.250@hotmail.com>
2020-01-09 11:57:41 +0100

    Dogadaji bez filtera

--- app/src/main/java/com/unipu/hr/GoPlay/Dogadaji.java
+++ app/src/main/java/com/unipu/hr/GoPlay/Dogadaji.java
@@ -1,25 +1,40 @@
 package com.unipu.hr.GoPlay;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.View;
 import android.widget.ViewFlipper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Dogadaji extends AppCompatActivity {
+    private FirebaseUser currentFirebaseUser;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,50 @@ public class Dogadaji extends AppCompatActivity {
             public void onClick(View view) {
                 BottomSheetDialogFragment bottomSheetDialogFragment = NavigacijaFragment.newInstance();
                 bottomSheetDialogFragment.show(getSupportFragmentManager(), "Bottom Sheet Dialog Fragment");
+            }
+        });
+
+                db = FirebaseFirestore.getInstance();
+                final Context hContex2 = this;
+                db.collection("dogadaji")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    List<item> mlist = new ArrayList<>();
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d("uspio", document.getId() + " => " + document.getData());
+                                        mlist.add(document.toObject(item.class));
+
+                                    }
+                                    RecyclerView recyclerView = findViewById(R.id.rv_list_2);
+                                    Adapter adapter = new Adapter(hContex2,mlist);
+                                    adapter.setOnItemClickListener(new Adapter.ClickListener() {
+                                        @Override
+                                        public void onItemClick(int position, View v,List<String> mData) {
+                                            Log.d("click", "onItemClick position: " + position);
+                                            Intent myIntent = new Intent(Dogadaji.this, Sudionici.class);
+                                            myIntent.putStringArrayListExtra("sudionici", (ArrayList<String>)mData);
+                                            myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                            startActivity(myIntent);
+                                        }
+
+                                        @Override
+                                        public void onItemLongClick(int position, View v,List<String> mData) {
+                                            Log.d("click", "onItemLongClick pos = " + position);
+                                            Intent myIntent = new Intent(Dogadaji.this, Sudionici.class);
+                                            myIntent.putStringArrayListExtra("sudionici",(ArrayList<String>) mData);
+                                            myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                            startActivity(myIntent);
+                                        }
+                                    });
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(hContex2));
+                                } else {
+                                    Log.w("nije uspio", "Error getting documents.", task.getException());
+                                }
+
 
             }
         });
--- app/src/main/java/com/unipu/hr/GoPlay/Home.java
+++ app/src/main/java/com/unipu/hr/GoPlay/Home.java
@@ -26,8 +26,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
