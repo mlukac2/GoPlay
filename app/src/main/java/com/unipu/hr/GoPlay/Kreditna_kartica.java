83cfd5dc929992790bb65928e5cb9bd419031248
mlukac2 <matteo.250@hotmail.com>
2020-01-22 21:43:50 +0100

    novcanik

--- app/src/main/java/com/unipu/hr/GoPlay/Kreditna_kartica.java
+++ app/src/main/java/com/unipu/hr/GoPlay/Kreditna_kartica.java
@@ -2,8 +2,13 @@ package com.unipu.hr.GoPlay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Kreditna_kartica extends AppCompatActivity {
 
@@ -11,9 +16,19 @@ public class Kreditna_kartica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kreditna_kartica);
+        final SharedPreferences preferences = getSharedPreferences("preferences",
+                MODE_PRIVATE);
+        final String userID = preferences.getString("user_id", "0");
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
         findViewById(R.id.kreditna_ok).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                TextView txtNovac = findViewById(R.id.txtNovac);
+                int novac = Integer.parseInt(txtNovac.getText().toString());
+                db.collection("korisnici").document(userID).update("novac", novac);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putLong("novac", novac);
+                editor.commit();
                 finish();
             }});
     }
